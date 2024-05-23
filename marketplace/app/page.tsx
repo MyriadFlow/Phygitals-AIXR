b643ea20cc9c06aa2e96262abe85e29bde5c6702
ss251 <ss251@uw.edu>
2024-05-24 05:10:01 +0530

    initial commit

---
+++ marketplace/app/page.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24 bg-background-light dark:bg-background-dark text-primary-dark dark:text-primary-light">
+    </div>
+  );
+}
