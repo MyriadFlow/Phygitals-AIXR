4fe928d904639de5acf3e0fa24755e68f982a955
ss251 <ss251@uw.edu>
2024-05-24 09:24:14 +0530

    chore: update build config

--- marketplace/next.config.mjs
+++ marketplace/next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    distDir: 'dist',
     webpack: config => {
       config.externals.push('pino-pretty', 'lokijs', 'encoding')
       return config
     }
   }
-
-export default nextConfig;
+  
+  export default nextConfig;
