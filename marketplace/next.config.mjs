18a3129fead246b4a8cc0768e7e573176c2f81dc
ss251 <ss251@uw.edu>
2024-05-27 22:22:47 +0530

    chore: add image domain next config

--- marketplace/next.config.mjs
+++ marketplace/next.config.mjs
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     webpack: config => {
       config.externals.push('pino-pretty', 'lokijs', 'encoding')
       return config
-    }
+    },
+    images: {
+      domains: ['qcntguqlgvrnctpcvjle.supabase.co'],
+    },
   }
   
   export default nextConfig;
