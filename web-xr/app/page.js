ad2dbc56dd5a8dab56d778d38f582f3521e0faf4
Adetayo <tuneymitch@gmail.com>
2024-06-01 15:18:49 +0100

    feat: webxr

---
+++ web-xr/app/page.js
@@ -0,0 +1,38 @@
+import { Assistant } from '@/components/assistant'
+import Link from 'next/link'
+
+async function fetchNfts() {
+	const res = await fetch('http://localhost:3000/api/products', {
+		cache: 'no-cache',
+	})
+
+	if (!res.ok) throw new Error('something went wrong')
+
+	const nftdata = await res.json()
+	return nftdata
+}
+
+export default async function Home() {
+	const nfts = await fetchNfts()
+
+	return (
+		<main className='h-screen flex flex-wrap justify-center items-center gap-4 bg-cyan-700'>
+			{nfts.map((item) => (
+				<Link
+					key={item.id}
+					href={`/collection/${item.id}`}
+					className='size-40 p-4 rounded transition-all bg-fuchsia-600 hover:bg-red-700 text-white cursor-pointer'
+				>
+					<div className='flex flex-col gap-4'>
+						<h1>{item.name}</h1>
+						<p className='truncate'>{item.description}</p>
+					</div>
+				</Link>
+			))}
+
+			<Assistant />
+		</main>
+	)
+}
+
+// sk - shI2gv25dRL85mPztStqT3BlbkFJwtgA1D89f4TB0heDyJdX
---
+++ web-xr/components/assistant.jsx
@@ -0,0 +1,51 @@
+'use client'
+import { useEffect } from 'react'
+import OpenAI from 'openai'
+
+export const Assistant = () => {
+	const openai = new OpenAI({
+		apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
+		dangerouslyAllowBrowser: true,
+	})
+
+	async function main() {
+		const assistant = await openai.beta.assistants.create({
+			name: 'Math Tutor',
+			instructions:
+				'You are a personal math tutor. Write and run code to answer math questions.',
+			tools: [{ type: 'code_interpreter' }],
+			model: 'gpt-4o',
+		})
+
+		console.log(assistant)
+	}
+
+	useEffect(() => {
+		main()
+	}, [])
+
+	main()
+
+	// const thread = await openai.beta.threads.create()
+
+	// const message = await openai.beta.threads.messages.create(thread.id, {
+	// 	role: 'user',
+	// 	content: 'I need to solve the equation `3x + 11 = 14`. Can you help me?',
+	// })
+
+	// let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
+	// 	assistant_id: assistant.id,
+	// 	instructions:
+	// 		'Please address the user as Jane Doe. The user has a premium account.',
+	// })
+
+	// if (run.status === 'completed') {
+	// 	const messages = await openai.beta.threads.messages.list(run.thread_id)
+	// 	for (const message of messages.data.reverse()) {
+	// 		console.log(`${message.role} > ${message.content[0].text.value}`)
+	// 	}
+	// } else {
+	// 	console.log(run.status)
+	// }
+	return <div>Assistant</div>
+}
