d272b8f4b20e9e41aa73b91d1e46ec8372c257b7
ss251 <ss251@uw.edu>
2024-06-02 18:49:25 +0530

    feat: add download glb, view and mint links

--- marketplace/app/dashboard/page.tsx
+++ marketplace/app/dashboard/page.tsx
@@ -1,8 +1,9 @@
-'use client';
-import { useEffect, useState } from 'react';
-import { supabase } from '../lib/supabaseClient';
-import { useAccount } from 'wagmi';
-import Image from 'next/image';
+"use client";
+import { useEffect, useState } from "react";
+import { supabase } from "../lib/supabaseClient";
+import { useAccount } from "wagmi";
+import Image from "next/image";
+import Link from "next/link";
 
 interface Avatar {
   id: string;
@@ -11,13 +12,18 @@ interface Avatar {
   attributes: Record<string, any>;
 }
 
+const NERO_MINT_URL = process.env.NEXT_NERO_MINT_URL || "";
+
 export default function Page() {
   const { address } = useAccount();
   const [avatars, setAvatars] = useState<Avatar[]>([]);
 
   useEffect(() => {
     const fetchAvatars = async () => {
-      const { data, error } = await supabase.from('avatars').select('*').eq('owner', address);
+      const { data, error } = await supabase
+        .from("avatars")
+        .select("*")
+        .eq("owner", address);
       if (error) {
         console.error(error);
       } else if (data) {
@@ -34,7 +40,10 @@ export default function Page() {
     <div className="px-20">
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {avatars.map((avatar) => (
-          <div key={avatar.id} className="border rounded-lg p-4 shadow-lg bg-white">
+          <div
+            key={avatar.id}
+            className="border rounded-lg p-4 shadow-lg bg-white"
+          >
             <Image
               src={avatar.image_url}
               alt="Avatar"
@@ -47,7 +56,14 @@ export default function Page() {
               height={200}
               className="rounded-md"
             />
-            <pre className="text-left bg-gray-100 p-2 rounded-md text-primary-dark">{JSON.stringify(avatar.attributes, null, 2)}</pre>
+            <pre className="text-left bg-gray-100 p-2 rounded-md text-primary-dark">
+              {JSON.stringify(avatar.attributes, null, 2)}
+            </pre>
+            <Link href={NERO_MINT_URL} target="_blank">
+              <button className="bg-primary-dark hover:bg-background-dark text-white font-bold py-2 px-4 rounded mt-4">
+                MINT NFT
+              </button>
+            </Link>
           </div>
         ))}
       </div>
