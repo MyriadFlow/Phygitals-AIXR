c2fbc018d1d5c9bd7a9f9c5e97c1ceb6093f064a
Adetayo <tuneymitch@gmail.com>
2024-06-02 16:17:23 +0100

    feat: lilipafd

--- web-xr/components/assistant.jsx
+++ web-xr/components/assistant.jsx
@@ -1,45 +1,32 @@
 'use client'
 import { useEffect } from 'react'
-import OpenAI from 'openai'
 
-export const Assistant = () => {
-	const openai = new OpenAI({
-		apiKey: process.env.NEXT_PUBLIC_OPENAI_KEY,
-		dangerouslyAllowBrowser: true,
-	})
+export const Assistant = ({ prompt }) => {
+	const [responseData, setResponseData] = useState(null)
+	const [isLoading, setIsLoading] = useState(false)
+	const [error, setError] = useState(null)
 
-	async function main() {
-		const assistant = await openai.beta.assistants.create({
-			name: 'Math Tutor',
-			instructions:
-				'You are a personal math tutor. Write and run code to answer math questions.',
-			tools: [{ type: 'code_interpreter' }],
-			model: 'gpt-4o',
-		})
+	const handleClick = async () => {
+		setIsLoading(true)
+		setError(null)
 
-		console.log(assistant)
+		try {
+			const response = await fetch('http://js-cli-wrapper.lilypad.tech', {
+				method: 'POST',
+				body: JSON.stringify({
+					pk: '0940934862b89b7a77ee21df96288ab0f1c78c27eabee6d5020e6bf95f3202f7',
+					module: 'ollama-pipeline:llama3-8b-lilypad1',
+					inputs: "-i Prompt='{your relevant prompt here}'",
+					opts: { stream: true },
+				}),
+			})
+			setResponseData(response.data)
+		} catch (error) {
+			setError(error)
+		} finally {
+			setIsLoading(false)
+		}
 	}
 
-	// const thread = await openai.beta.threads.create()
-
-	// const message = await openai.beta.threads.messages.create(thread.id, {
-	// 	role: 'user',
-	// 	content: 'I need to solve the equation `3x + 11 = 14`. Can you help me?',
-	// })
-
-	// let run = await openai.beta.threads.runs.createAndPoll(thread.id, {
-	// 	assistant_id: assistant.id,
-	// 	instructions:
-	// 		'Please address the user as Jane Doe. The user has a premium account.',
-	// })
-
-	// if (run.status === 'completed') {
-	// 	const messages = await openai.beta.threads.messages.list(run.thread_id)
-	// 	for (const message of messages.data.reverse()) {
-	// 		console.log(`${message.role} > ${message.content[0].text.value}`)
-	// 	}
-	// } else {
-	// 	console.log(run.status)
-	// }
 	return <div>Assistant</div>
 }
